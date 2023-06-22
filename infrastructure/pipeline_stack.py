712d15f5c0f03b1b55fc508c4773e60fc77a1cbf
Franco Abregu <fabregu@amazon.com>
2023-06-22 09:56:13 -0300

    pylint and pytest steps added

--- infrastructure/pipeline_stack.py
+++ infrastructure/pipeline_stack.py
@@ -4,13 +4,15 @@ from aws_cdk.aws_codecommit import Repository
 from aws_cdk.pipelines import (
     CodePipeline,
     CodePipelineSource,
-    ShellStep
+    ShellStep,
+    CodeBuildStep
 )
-#from aws_cdk.aws_codebuild import (
-#    BuildEnvironment,
-#    LinuxBuildImage,
-#    ComputeType
-#)
+from aws_cdk.aws_codebuild import (
+    BuildEnvironment,
+    LinuxBuildImage,
+    ComputeType
+)
+from aws_cdk.aws_codebuild import BuildSpec
 from .deploy_stage import DeployStage
 
 class PipelineStack(Stack):
@@ -23,9 +25,60 @@ class PipelineStack(Stack):
             f'arn:aws:codecommit:{self.region}:{self.account}:{app_name}'
         )
 
+        cdk_install_commands =  ["npm install -g aws-cdk",
+            "python3 -m venv .env",
+            "chmod +x .env/bin/activate",
+            ". .env/bin/activate",
+            "pip3 install -r requirements.txt",
+            "pip3 install -r requirements-dev.txt"
+        ]
+
         source_stage = CodePipelineSource.code_commit(
             repository,
             "main"
+        )
+
+        environment = BuildEnvironment(
+            build_image = LinuxBuildImage.STANDARD_7_0,
+            compute_type = ComputeType.SMALL
+        )
+
+        pylint_step = CodeBuildStep(
+            "Linter",
+            input = source_stage,
+            build_environment = environment,
+            project_name = f"{app_name}-pipeline-linter",
+            install_commands = cdk_install_commands,
+            commands = [
+                "find . -name '*.py' ! -path './.env/*' ! -path './cdk.out/*' ! -path './node_modules/*' | xargs pylint"
+            ]
+        )
+
+        pytest_step = CodeBuildStep(
+            "UnitTests",
+            input = source_stage,
+            build_environment = environment,
+            project_name = f"{app_name}-pipeline-unit-tests",
+            install_commands = cdk_install_commands,
+            commands = [
+                "coverage erase && python3 -m coverage run --branch -m pytest -v && coverage report",
+                "python3 -m coverage xml -i -o test-results/coverage.xml",
+                "python3 -m pytest --junitxml=test-results/results.xml"
+            ],
+            partial_build_spec = BuildSpec.from_object({
+                "reports": {
+                    "unit_tests_reports": {
+                        "files": "results.xml",
+                        "base-directory": "test-results",
+                        "file-format": "JUNITXML"
+                    },
+                    "coverage_reports": {
+                        "files": "coverage.xml",
+                        "base-directory": "test-results",
+                        "file-format": "COBERTURAXML"
+                    }
+                }
+            })
         )
 
         pipeline = CodePipeline(
@@ -36,19 +89,12 @@ class PipelineStack(Stack):
             synth = ShellStep(
                 "Synth",
                 input = source_stage,
-                env={
+                env = {
                     "ACCOUNT" : self.account,
                     "REGION": self.region
                 },
-                install_commands=[
-                    "npm install -g aws-cdk",
-                    "python3 -m venv .env",
-                    "chmod +x .env/bin/activate",
-                    ". .env/bin/activate",
-                    "pip3 install -r requirements.txt",
-                    "pip3 install -r requirements-dev.txt"
-                    ],
-                commands=[
+                install_commands = cdk_install_commands,
+                commands = [
                     "cdk synth"
                 ]
             )
@@ -60,5 +106,8 @@ class PipelineStack(Stack):
                 'QA',
                 app_name = app_name
             ),
-            pre = []
+            pre = [
+                pylint_step,
+                pytest_step
+            ]
         )
