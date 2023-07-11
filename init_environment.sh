761648c5f4ebc4d08e45f061c04b148f996648eb
Franco Abregu <fabregu@amazon.com>
2023-07-11 09:56:45 -0300

    init_environment.sh code simplicity

--- init_environment.sh
+++ init_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+REPO_NAME=cdk8s-samples
 cdk bootstrap aws://$ACCOUNT/$REGION
 aws codecommit create-repository --repository-name $REPO_NAME --region $REGION
-git remote add origin codecommit::$REGION://$PROFILE@$REPO_NAME
+git remote add origin codecommit::$REGION://$REPO_NAME
 git push --set-upstream origin main
