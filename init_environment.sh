458f92bc6f74e50934e90f9a2acde03b080b9c3c
Franco Abregu <fabregu@amazon.com>
2023-07-11 10:34:32 -0300

    init_environment.sh fixes

--- init_environment.sh
+++ init_environment.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 REPO_NAME=cdk8s-samples
+python3 -m venv .venv
+chmod +x .venv/bin/activate
+. .venv/bin/activate
+pip3 install -r requirements.txt
 cdk bootstrap aws://$ACCOUNT/$REGION
 aws codecommit create-repository --repository-name $REPO_NAME --region $REGION
+git remote rm origin
 git remote add origin codecommit::$REGION://$REPO_NAME
 git push --set-upstream origin main
